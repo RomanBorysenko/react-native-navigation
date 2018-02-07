8cf3804384556bafd7ef5b5d7ff50dbcb5fd0bf3
bdavid68 <b.david6008@gmail.com>
2018-02-08 01:32:07 +1000

    Replace scene feature in iOS, lightbox animation

---
+++ android/app/src/main/java/com/reactnativenavigation/params/LightBoxParams.java
@@ -0,0 +1,12 @@
+package com.reactnativenavigation.params;
+
+public class LightBoxParams {
+    public String screenId;
+    public NavigationParams navigationParams;
+    public StyleParams.Color backgroundColor;
+    public boolean tapBackgroundToDismiss;
+    public boolean overrideBackPress;
+    public int adjustSoftInput;
+    public String animationIn; // slideLeftIn, slideRightIn
+    public String animationOut; // slideLeftOut, slideRightOut
+}
